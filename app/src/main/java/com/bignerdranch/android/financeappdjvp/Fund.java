c99d1679e459384f32d2733cafbac96ae2721e4f
huyviet1995 <vietdanghuy.95@gmail.com>
2016-04-08 23:37:56 -0500

    Updates from the backend team

---
+++ app/src/main/java/com/bignerdranch/android/financeappdjvp/Fund.java
@@ -0,0 +1,31 @@
+package com.bignerdranch.android.financeappdjvp;
+
+import java.util.UUID;
+
+/**
+ * Created by huyviet1995 on 4/8/16.
+ */
+public class Fund {
+    private String mTitle;
+    private UUID mId;
+    //Questions: Properties of a fund
+    public Fund(String title) {
+        mTitle = title;
+    }
+    //get Id
+    public UUID getId() {
+        return mId;
+    }
+    //set Id
+    public void setId(UUID newId) {
+        mId = newId;
+    }
+    //get name of the fund
+    public String getTitle() {
+        return mTitle;
+    }
+    //set title of the fund
+    public void setTitle(String newTitle) {
+        mTitle = newTitle;
+    }
+}
--- app/src/main/java/com/bignerdranch/android/financeappdjvp/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/financeappdjvp/MainActivity.java
@@ -1,13 +1,19 @@
 package com.bignerdranch.android.financeappdjvp;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected Fragment createFragment() {
+        return FinanceMainFragment.newInstance();
+    }
+
+    /*@Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_main_screen);
-    }
+    }*/
 }
